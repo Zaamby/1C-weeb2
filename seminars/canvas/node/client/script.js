d2868953598d78d520a250ddcc42fd01bf61370a
Daniel Zelenov <zaaamby000@gmail.com>
2017-10-14 15:57:57 +0300

    Add node.js example

---
+++ seminars/canvas/node/client/script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("request")
+button.addEventListener("click", function(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:591/", true);     
+    xhr
+})
